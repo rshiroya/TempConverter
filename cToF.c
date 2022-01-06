f2580a08c34eb9c73b2f7255a3d4fb7a5b47c422
rshiroya <71335170+rshiroya@users.noreply.github.com>
2022-01-06 00:58:12 -0500

    Update cToF.c

--- cToF.c
+++ cToF.c
@@ -1,58 +1,112 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * C program to convert temperature from degree celsius to fahrenheit
+ */  
 #include <stdio.h>
 #include <conio.h>
+  
+#define LOWER_LIMIT 0
+#define HIGHER_LIMIT 1000
+  
+int
+main (void)
+{
+  
+ 
+float unit;
+unit = 0;
+  
+float cel, fahr;
+  
+cel = 0;
+  
+fahr = 0;
+   
+    /* Option selection based on user preferences */ 
 
-            #define LOWER_LIMIT 0
-            #define HIGHER_LIMIT 1000
+printf ("\n\n------------------------------------");
+      
+printf ("\n1. Convert Celsius to Fahrenheit: ");
+      
+printf ("\n2. convert Fahrenheit to Celsius: ");
+      
+printf ("\n3. Exit");
+      
+printf ("\nPlease select from the following options: ");
+      
+scanf ("%f", &unit);
+      
+ 
+	/* If user selects option 1 */ 
+	if (unit == 1)
+	{
+	  
+printf ("You have chosen the unit Celsius");
+	  
+printf
+	    ("\nPlease enter the amount of Celsius you want to convert to Fahrenheit: ");
+	  
+scanf ("%f", &cel);
+	  
+float f = 0;
+	  
+	    /* celsius to fahrenheit conversion formula */ 
+	    f = ((9.0 / 5.0) * cel) + 32;
+	  
+printf
+	    ("After using the formula of conversion and converting given value of celsius to fahrenheit, the value of fahrenheit is: %f",
+	     f);
+	  
+	    // redirects to the main() method
+	    
+	
+}
+      
+ 
+	/* If user selects option 2 */ 
+	else if (unit == 2)
+	{
+	  
+printf ("You have chosen the unit Fahrenheit");
+	  
+printf
+	    ("\nPlease enter the amount of Fahrenheit you want to convert to Celsius: ");
+	  
+scanf ("%f", &fahr);
+	  
+float c = 0;
+	  
+	    /* fahrenheit to celsius conversion formula */ 
+	    c = (5.0 * (fahr - 32)) / 9.0;
+	  
+printf
+	    ("After using the formula of conversion and converting given value of fahrenheit to celsius, the value of celsius is: %f",
+	     c);
+	  
+	    // redirects to the main() method
+	    
+	
+}
+      
+	/* If user selects option 3 */ 
+	else if (unit == 3)
+	{
+	  
+printf("Thank you for using the program! :) :) Happy to Assist Anytime!");
+return 0;	
+}
 
-            int main(void) {
+    else{
+     printf("Please Try again: ");
+     unit = 0;
+     
+     main();
+     return 0;
+ }
 
-                char unit;
+main();
+  
+return 0;
 
-                float cel, fahr;
-                cel = 0;
-                fahr = 0;
-                    /* Read in lower, higher limit and step */
-
-                   printf("\n\n------------------------------------");
-                   printf("\n1. Convert Celsius to Fahrenheit: ");
-                   printf("\n2. convert Fahrenheit to Celsius: ");
-                   printf("\n3. Exit");
-                   printf("\nPlease select from the following options: ");
-                    scanf("%c", &unit);
+}
 
 
-                    if(unit == '1'){
-                        printf("You have chosen the unit Celsius");
-                        printf("\nPlease enter the amount of Celsius you want to convert to Fahrenheit: ");
-                        scanf("%f", &cel);
-                        float f = 0;
-                        f = ((9.0/5.0)*cel)+32;
-                        printf("After using the formula of conversion and converting given value of celsius to fahrenheit, the value of fahrenheit is: %f", f);
-                        main();
-                    }
-                    else if(unit == '2'){
-                        printf("You have chosen the unit Fahrenheit");
-                        printf("\nPlease enter the amount of Fahrenheit you want to convert to Celsius: ");
-                        scanf("%f", &fahr);
-                        float c = 0;
-                        c = (5.0*(fahr-32))/9.0;
-                        printf("After using the formula of conversion and converting given value of fahrenheit to celsius, the value of celsius is: %f", c);
-                        main();
-                    }
-                    else if(unit == '3'){
-                        printf("Thank you for using the program! :) :) Happy to Assist Anytime!");
-                    }
-                    else
-                        {
-
-                        main();
-
-                        }
-
-                    return 0;
-                }
